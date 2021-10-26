a2db915b9e16fc0b4a58827bfcf9333dc4ba41cf
Kamiel <kamieldevisser@gmail.com>
2021-10-26 15:17:59 +0200

    init commit

---
+++ Assets/Player.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rigidBody;
+
+    public float speedX;
+    public float gravityValue;
+
+    public bool invertGravity;
+
+    private Transform graphicsHolder;
+    private Transform physicsHolder;
+
+    private float boxColliderHeight;
+
+    private BoxCollider2D feet;
+
+    private bool grounded;
+
+    private float Gravity
+    {
+        get
+        {
+            return invertGravity ? gravityValue : -gravityValue;
+        }
+    }    
+
+    private void Start()
+    {
+        graphicsHolder = transform.Find("GFX");
+        physicsHolder = transform.Find("Physics");
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxColliderHeight = physicsHolder.transform.Find("Body").GetComponent<BoxCollider2D>().size.y;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvertGravity();
+        }
+    }
+
+    private void InvertGravity()
+    {
+        invertGravity = !invertGravity;
+
+        int signOffset = invertGravity ? 1 : -1;
+
+        transform.position = new Vector3(transform.position.x, transform.position.y + signOffset * boxColliderHeight, transform.position.z);
+        graphicsHolder.localScale = new Vector3(graphicsHolder.localScale.x, -graphicsHolder.localScale.y, graphicsHolder.localScale.z);
+        physicsHolder.localScale = new Vector3(physicsHolder.localScale.x, -physicsHolder.localScale.y, physicsHolder.localScale.z);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!grounded)
+        {
+            rigidBody.MovePosition(transform.position + new Vector3(0, Gravity * Time.deltaTime));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        grounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        grounded = false;
+    }
+}
